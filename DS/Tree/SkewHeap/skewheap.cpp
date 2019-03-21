87cdb20360615bcb897baa0b5ebe87809602211e
Rushin <chiefpi@outlook.com>
2019-03-21 15:19:36 +0800

    SkewHeap followup

--- DS/Tree/SkewHeap/skewheap.cpp
+++ DS/Tree/SkewHeap/skewheap.cpp
@@ -31,10 +31,11 @@ public:
     CSkewHeap();
     ~CSkewHeap();
     void Insert(T key);
-    void Delete(T key);
+    T PopMin();
+    CSkewHeap &Union(CSkewHeap &sh);
 private:
     SkewNode<T> *_Merge(SkewNode<T> *root_1, SkewNode<T> *root_2);
-    void _Purge(SkewNode<T> *root);
+    // void _Purge(SkewNode<T> *root);
 };
 
 template<class T, class CompareFunction>
@@ -46,29 +47,39 @@ CSkewHeap<T, CompareFunction>::CSkewHeap()
 template<class T, class CompareFunction>
 CSkewHeap<T, CompareFunction>::~CSkewHeap()
 {
-    _Purge(root);
-}
-
-template<class T, class CompareFunction>
-void CSkewHeap<T, CompareFunction>::_Purge(SkewNode<T> *root)
-{
-
+    // _Purge(root);
+    while (root != nullptr)
+        PopMin(root);
 }
 
 template<class T, class CompareFunction>
 void CSkewHeap<T, CompareFunction>::Insert(T key)
 {
-    SkewNode newNode(key);
+    SkewNode<T> newNode(key);
     _Merge(this->root, newNode);
 }
 
 template<class T, class CompareFunction>
-void CSkewHeap<T, CompareFunction>::Delete(T key)
+T CSkewHeap<T, CompareFunction>::PopMin()
 {
-    
+    T minKey = root->key;
+    SkewNode<T> *left = root->leftNode;
+    SkewNode<T> *right = root->rightNode;
+    delete root;
+    _Merge(left, right);
+
+    return minKey;
 }
 
+template<class T, class CompareFunction>
+CSkewHeap<T, CompareFunction> &CSkewHeap<T, CompareFunction>::Union(CSkewHeap<T, CompareFunction> &sh)
+{
+    CSkewHeap<T, CompareFunction> newSkewHeap;
+    newSkewHeap->root = _Merge(this->root, sh.root);
+    return newSkewHeap;
+}
 
+// Recursive
 template<class T, class CompareFunction>
 SkewNode<T> *CSkewHeap<T, CompareFunction>::_Merge(SkewNode<T> *root_1, SkewNode<T> *root_2)
 {
@@ -81,7 +92,7 @@ SkewNode<T> *CSkewHeap<T, CompareFunction>::_Merge(SkewNode<T> *root_1, SkewNode
     if (secondRoot == nullptr)
         return firstRoot;
 
-    if(sh_compare->Less(firstRoot->key, secondRoot->key))
+    if(CompareFunction.Less(firstRoot->key, secondRoot->key))
     {
         SkewNode<T>* tempHeap = firstRoot->rightNode;
         firstRoot->rightNode = firstRoot->leftNode;
@@ -92,8 +103,31 @@ SkewNode<T> *CSkewHeap<T, CompareFunction>::_Merge(SkewNode<T> *root_1, SkewNode
         return _Merge(secondRoot, firstRoot);
 }
 
+// template<class T, class CompareFunction>
+// void CSkewHeap<T, CompareFunction>::_Purge(SkewNode<T> *root)
+// {
+
+// }
+
+class IntegerCompare
+{
+public:
+    bool Less(int a, int b)
+    {
+        return a < b;
+    }
+};
+
 int main()
 {
+    CSkewHeap<int, IntegerCompare> sh1, sh2;
+    sh1.Insert(1);
+    sh1.Insert(3);
+    sh1.Insert(5);
+    sh2.Insert(7);
+    sh2.Insert(2);
+    CSkewHeap<int, IntegerCompare> sh = sh1.Union(sh2);
+    std::cout << sh1.PopMin() << std::endl;
 
     return 0;
 }
