8cb68228a7d26c71f50d27adc71986a15370386e
tenpc <chiefpan0@gmail.com>
2020-09-18 23:01:02 +0800

    change directory structures

--- OJ/hihoCoder/1053_transition.cpp
+++
@@ -1,8 +0,0 @@
-// image smoothing with kernel sized R
-#include <cstdio>
-
-int main(int argc, char const *argv[])
-{
-	
-	return 0;
-}
--- OJ/hihoCoder/1870_DAG.cpp
+++
@@ -1,4 +0,0 @@
-// ACM/ICPC BJ 2018 A
-// Jin Yong's Wukong Ranking List
-#include <cstdio>
-
